bb6aa2388fb3c37e19f33d3334a9249b8863b34f
ysys19911004 <yang.shuo2@husky.neu.edu>
2018-11-29 20:17:06 -0500

    change

--- src/Cube2.java
+++ src/Cube2.java
@@ -1,3 +1,5 @@
+import static java.util.Objects.hash;
+
 import java.util.Random;
 
 public class Cube2 {
@@ -5,9 +7,10 @@ public class Cube2 {
   //stores the 2*2*2 cube status
 
   int[][] status;
-
   String actions;
-
+  //initialize with
+  //f:face c: cube
+  //1111,2222,3333,4444,5555,6666
   public Cube2() {
     status = new int[4][6];
     for (int f = 0; f < 6; f++) {
@@ -15,9 +18,25 @@ public class Cube2 {
         status[c][f] = f + 1;
       }
     }
-actions="";
+    actions = "0";
   }
 
+  public Cube2(int i){
+    status = new int[4][6];
+    actions = "0";
+  }
+
+  public boolean equals(Object o){
+    if(!(o instanceof Cube2)){
+      return false;
+    }
+    Cube2 another = (Cube2)o;
+    return this.status.equals(another.status);
+  }
+
+  public int hashCode(){
+    return hash(this.status);
+  }
 
 }
 
--- src/Search.java
+++ src/Search.java
@@ -1,12 +1,18 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
+import java.util.Set;
 
 public class Search {
 
   Cube2 startState;
 
-  static String[] actionList = new String[]{"R", "R'", "L", "L'", "F", "F'", "U", "U'", "B", "B'",
-      "D", "D'"};
-
+  static String[] actionList1 = new String[]{"R", "r", "L", "l"};
+  static String[] actionList2 = new String[]{"F", "f", "B", "b"};
+  static String[] actionList3 = new String[]{"U", "u", "D", "d"};
+  static String[] actionListAll = new String[]{"R", "r", "L", "l", "F", "f", "B", "b", "U",
+      "u", "D", "d"};
 
   public Search(Cube2 startState) {
     this.startState = startState;
@@ -16,32 +22,33 @@ public class Search {
     Random ran = new Random();
     for (int i = 0; i < 50; i++) {
       int index = ran.nextInt(12);
-      this.shift(this.startState, actionList[index]);
+      this.startState = this.shift(this.startState, actionListAll[index]);
     }
   }
 
   public Cube2 shift(Cube2 cube1, String action) {
-    Cube2 cube = new Cube2();
-    cube.status = cube1.status;
-    cube.actions+=(action+", ");
 
-    switch(action.toUpperCase()){
-      case("R"):
+    Cube2 cube = new Cube2(0);
+    cube.status = cube1.status;
+    cube.actions += (cube1.actions + action);
+
+    switch (action.toUpperCase()) {
+      case ("R"):
 
         int Rfront1 = cube.status[1][2];
         int Rfront3 = cube.status[3][2];
 
-        cube.status[1][2]=cube.status[1][5];
-        cube.status[3][2]=cube.status[3][5];
+        cube.status[1][2] = cube.status[1][5];
+        cube.status[3][2] = cube.status[3][5];
 
-        cube.status[1][5]=cube.status[2][4];
-        cube.status[3][5]=cube.status[0][4];
+        cube.status[1][5] = cube.status[2][4];
+        cube.status[3][5] = cube.status[0][4];
 
-        cube.status[2][4]=cube.status[1][0];
-        cube.status[0][4]=cube.status[3][0];
+        cube.status[2][4] = cube.status[1][0];
+        cube.status[0][4] = cube.status[3][0];
 
-        cube.status[1][0]=Rfront1;
-        cube.status[3][0]=Rfront3;
+        cube.status[1][0] = Rfront1;
+        cube.status[3][0] = Rfront3;
 
         int Rside1 = cube.status[0][3];
         cube.status[0][3] = cube.status[2][3];
@@ -51,22 +58,22 @@ public class Search {
 
         break;
 
-      case("R'"):
+      case ("r"):
 
         int Rpfront1 = cube.status[1][2];
         int Rpfront3 = cube.status[3][2];
 
-        cube.status[1][2]=cube.status[1][0];
-        cube.status[3][2]=cube.status[3][0];
+        cube.status[1][2] = cube.status[1][0];
+        cube.status[3][2] = cube.status[3][0];
 
-        cube.status[1][0]=cube.status[2][4];
-        cube.status[3][0]=cube.status[0][4];
+        cube.status[1][0] = cube.status[2][4];
+        cube.status[3][0] = cube.status[0][4];
 
-        cube.status[2][4]=cube.status[1][5];
-        cube.status[0][4]=cube.status[3][5];
+        cube.status[2][4] = cube.status[1][5];
+        cube.status[0][4] = cube.status[3][5];
 
-        cube.status[1][5]=Rpfront1;
-        cube.status[3][5]=Rpfront3;
+        cube.status[1][5] = Rpfront1;
+        cube.status[3][5] = Rpfront3;
 
         int Rpside1 = cube.status[0][3];
         cube.status[0][3] = cube.status[1][3];
@@ -75,21 +82,21 @@ public class Search {
         cube.status[2][3] = Rpside1;
         break;
 
-      case("L"):
+      case ("L"):
         int Lfront0 = cube.status[0][2];
         int Lfront2 = cube.status[2][2];
 
-        cube.status[0][2]=cube.status[0][0];
-        cube.status[2][2]=cube.status[2][0];
+        cube.status[0][2] = cube.status[0][0];
+        cube.status[2][2] = cube.status[2][0];
 
-        cube.status[0][0]=cube.status[3][4];
-        cube.status[2][0]=cube.status[1][4];
+        cube.status[0][0] = cube.status[3][4];
+        cube.status[2][0] = cube.status[1][4];
 
-        cube.status[3][4]=cube.status[0][5];
-        cube.status[1][4]=cube.status[2][5];
+        cube.status[3][4] = cube.status[0][5];
+        cube.status[1][4] = cube.status[2][5];
 
-        cube.status[0][5]=Lfront0;
-        cube.status[2][5]=Lfront2;
+        cube.status[0][5] = Lfront0;
+        cube.status[2][5] = Lfront2;
 
         int Lside1 = cube.status[0][1];
         cube.status[0][1] = cube.status[2][1];
@@ -98,22 +105,22 @@ public class Search {
         cube.status[1][1] = Lside1;
         break;
 
-      case("L'"): //////
+      case ("l"): //////
 
         int Lpfront0 = cube.status[0][2];
         int Lpfront2 = cube.status[2][2];
 
-        cube.status[0][2]=cube.status[0][5];
-        cube.status[2][2]=cube.status[2][5];
+        cube.status[0][2] = cube.status[0][5];
+        cube.status[2][2] = cube.status[2][5];
 
-        cube.status[0][5]=cube.status[3][4];
-        cube.status[2][5]=cube.status[1][4];
+        cube.status[0][5] = cube.status[3][4];
+        cube.status[2][5] = cube.status[1][4];
 
-        cube.status[3][4]=cube.status[0][0];
-        cube.status[1][4]=cube.status[2][0];
+        cube.status[3][4] = cube.status[0][0];
+        cube.status[1][4] = cube.status[2][0];
 
-        cube.status[0][0]=Lpfront0;
-        cube.status[2][0]=Lpfront2;
+        cube.status[0][0] = Lpfront0;
+        cube.status[2][0] = Lpfront2;
 
         int Lpside1 = cube.status[0][1];
         cube.status[0][1] = cube.status[2][1];
@@ -123,21 +130,21 @@ public class Search {
 
         break;
 
-      case("U"):
+      case ("U"):
         int Ufront0 = cube.status[0][2];
         int Ufront1 = cube.status[1][2];
 
-        cube.status[0][2]=cube.status[0][3];
-        cube.status[1][2]=cube.status[1][3];
+        cube.status[0][2] = cube.status[0][3];
+        cube.status[1][2] = cube.status[1][3];
 
-        cube.status[0][3]=cube.status[0][4];
-        cube.status[1][3]=cube.status[1][4];
+        cube.status[0][3] = cube.status[0][4];
+        cube.status[1][3] = cube.status[1][4];
 
-        cube.status[0][4]=cube.status[0][1];
-        cube.status[1][4]=cube.status[1][1];
+        cube.status[0][4] = cube.status[0][1];
+        cube.status[1][4] = cube.status[1][1];
 
-        cube.status[0][1]=Ufront0;
-        cube.status[1][1]=Ufront1;
+        cube.status[0][1] = Ufront0;
+        cube.status[1][1] = Ufront1;
 
         int Uside0 = cube.status[1][0];
         cube.status[1][0] = cube.status[0][0];
@@ -146,23 +153,21 @@ public class Search {
         cube.status[3][0] = Uside0;
         break;
 
-
-
-      case("U'"):
+      case ("u"):
         int Upfront0 = cube.status[0][2];
         int Upfront1 = cube.status[1][2];
 
-        cube.status[0][2]=cube.status[1][1];
-        cube.status[1][2]=cube.status[0][1];
+        cube.status[0][2] = cube.status[1][1];
+        cube.status[1][2] = cube.status[0][1];
 
-        cube.status[1][1]=cube.status[1][4];
-        cube.status[0][1]=cube.status[0][4];
+        cube.status[1][1] = cube.status[1][4];
+        cube.status[0][1] = cube.status[0][4];
 
-        cube.status[1][4]=cube.status[1][3];
-        cube.status[0][4]=cube.status[0][3];
+        cube.status[1][4] = cube.status[1][3];
+        cube.status[0][4] = cube.status[0][3];
 
-        cube.status[1][3]=Upfront1;
-        cube.status[0][3]=Upfront0;
+        cube.status[1][3] = Upfront1;
+        cube.status[0][3] = Upfront0;
 
         int Upside0 = cube.status[1][0];
         cube.status[1][0] = cube.status[3][0];
@@ -171,21 +176,21 @@ public class Search {
         cube.status[0][0] = Upside0;
         break;
 
-      case("F"):
+      case ("F"):
         int Ffront2 = cube.status[2][0];
         int Ffront3 = cube.status[3][0];
 
-        cube.status[2][0]=cube.status[3][1];
-        cube.status[3][0]=cube.status[1][1];
+        cube.status[2][0] = cube.status[3][1];
+        cube.status[3][0] = cube.status[1][1];
 
-        cube.status[3][1]=cube.status[1][5];
-        cube.status[1][1]=cube.status[0][5];
+        cube.status[3][1] = cube.status[1][5];
+        cube.status[1][1] = cube.status[0][5];
 
-        cube.status[1][5]=cube.status[0][3];
-        cube.status[0][5]=cube.status[2][3];
+        cube.status[1][5] = cube.status[0][3];
+        cube.status[0][5] = cube.status[2][3];
 
-        cube.status[0][3]=Ffront2;
-        cube.status[2][3]=Ffront3;
+        cube.status[0][3] = Ffront2;
+        cube.status[2][3] = Ffront3;
 
         int Fside2 = cube.status[0][2];
         cube.status[0][2] = cube.status[2][2];
@@ -194,21 +199,21 @@ public class Search {
         cube.status[1][2] = Fside2;
         break;
 
-      case("F'"):
+      case ("f"):
         int Fpfront2 = cube.status[2][0];
         int Fpfront3 = cube.status[3][0];
 
-        cube.status[2][0]=cube.status[0][3];
-        cube.status[3][0]=cube.status[2][3];
+        cube.status[2][0] = cube.status[0][3];
+        cube.status[3][0] = cube.status[2][3];
 
-        cube.status[0][3]=cube.status[1][5];
-        cube.status[2][3]=cube.status[0][5];
+        cube.status[0][3] = cube.status[1][5];
+        cube.status[2][3] = cube.status[0][5];
 
-        cube.status[1][5]=cube.status[3][1];
-        cube.status[0][5]=cube.status[1][1];
+        cube.status[1][5] = cube.status[3][1];
+        cube.status[0][5] = cube.status[1][1];
 
-        cube.status[3][1]=Fpfront2;
-        cube.status[1][1]=Fpfront3;
+        cube.status[3][1] = Fpfront2;
+        cube.status[1][1] = Fpfront3;
 
         int Fpside2 = cube.status[0][2];
         cube.status[0][2] = cube.status[1][2];
@@ -217,21 +222,21 @@ public class Search {
         cube.status[2][2] = Fpside2;
         break;
 
-      case("B"):
+      case ("B"):
         int Bfront0 = cube.status[0][0];
         int Bfront1 = cube.status[1][0];
 
-        cube.status[0][0]=cube.status[1][3];
-        cube.status[1][0]=cube.status[3][3];
+        cube.status[0][0] = cube.status[1][3];
+        cube.status[1][0] = cube.status[3][3];
 
-        cube.status[1][3]=cube.status[3][5];
-        cube.status[3][3]=cube.status[2][5];
+        cube.status[1][3] = cube.status[3][5];
+        cube.status[3][3] = cube.status[2][5];
 
-        cube.status[3][5]=cube.status[2][1];
-        cube.status[2][5]=cube.status[0][1];
+        cube.status[3][5] = cube.status[2][1];
+        cube.status[2][5] = cube.status[0][1];
 
-        cube.status[2][1]=Bfront0;
-        cube.status[0][1]=Bfront1;
+        cube.status[2][1] = Bfront0;
+        cube.status[0][1] = Bfront1;
 
         int Bside4 = cube.status[0][4];
         cube.status[0][4] = cube.status[2][4];
@@ -241,21 +246,21 @@ public class Search {
 
         break;
 
-      case("B'"):
+      case ("b"):
         int Bpfront0 = cube.status[0][0];
         int Bpfront1 = cube.status[1][0];
 
-        cube.status[0][0]=cube.status[2][1];
-        cube.status[1][0]=cube.status[0][1];
+        cube.status[0][0] = cube.status[2][1];
+        cube.status[1][0] = cube.status[0][1];
 
-        cube.status[2][1]=cube.status[2][5];
-        cube.status[0][1]=cube.status[3][5];
+        cube.status[2][1] = cube.status[2][5];
+        cube.status[0][1] = cube.status[3][5];
 
-        cube.status[2][5]=cube.status[3][3];
-        cube.status[3][5]=cube.status[1][3];
+        cube.status[2][5] = cube.status[3][3];
+        cube.status[3][5] = cube.status[1][3];
 
-        cube.status[3][3]=Bpfront1;
-        cube.status[1][3]=Bpfront0;
+        cube.status[3][3] = Bpfront1;
+        cube.status[1][3] = Bpfront0;
 
         int Bpside4 = cube.status[0][4];
         cube.status[0][4] = cube.status[1][4];
@@ -264,21 +269,21 @@ public class Search {
         cube.status[2][4] = Bpside4;
         break;
 
-      case("D"):
+      case ("D"):
         int Dfront2 = cube.status[2][2];
         int Dfront3 = cube.status[3][2];
 
-        cube.status[2][2]=cube.status[2][1];
-        cube.status[3][2]=cube.status[3][1];
+        cube.status[2][2] = cube.status[2][1];
+        cube.status[3][2] = cube.status[3][1];
 
-        cube.status[2][1]=cube.status[2][4];
-        cube.status[3][1]=cube.status[3][4];
+        cube.status[2][1] = cube.status[2][4];
+        cube.status[3][1] = cube.status[3][4];
 
-        cube.status[2][4]=cube.status[2][3];
-        cube.status[3][4]=cube.status[3][3];
+        cube.status[2][4] = cube.status[2][3];
+        cube.status[3][4] = cube.status[3][3];
 
-        cube.status[2][3]=Dfront2;
-        cube.status[3][3]=Dfront3;
+        cube.status[2][3] = Dfront2;
+        cube.status[3][3] = Dfront3;
 
         int Dside5 = cube.status[0][5];
         cube.status[0][5] = cube.status[2][5];
@@ -287,21 +292,21 @@ public class Search {
         cube.status[1][5] = Dside5;
         break;
 
-      case("D'"):
+      case ("d"):
         int Dpfront2 = cube.status[2][2];
         int Dpfront3 = cube.status[3][2];
 
-        cube.status[2][2]=cube.status[2][3];
-        cube.status[3][2]=cube.status[3][3];
+        cube.status[2][2] = cube.status[2][3];
+        cube.status[3][2] = cube.status[3][3];
 
-        cube.status[2][3]=cube.status[2][4];
-        cube.status[3][3]=cube.status[3][4];
+        cube.status[2][3] = cube.status[2][4];
+        cube.status[3][3] = cube.status[3][4];
 
-        cube.status[2][4]=cube.status[2][1];
-        cube.status[3][4]=cube.status[3][1];
+        cube.status[2][4] = cube.status[2][1];
+        cube.status[3][4] = cube.status[3][1];
 
-        cube.status[2][1]=Dpfront2;
-        cube.status[3][1]=Dpfront3;
+        cube.status[2][1] = Dpfront2;
+        cube.status[3][1] = Dpfront3;
 
         int Dpside5 = cube.status[0][5];
         cube.status[0][5] = cube.status[1][5];
@@ -314,7 +319,62 @@ public class Search {
     return cube;
   }
 
-  public String OptmisticSearch() {
-    return "";
+  public static String optmisticSearch(Search search) {
+    Queue<Cube2> fringe = new LinkedList<Cube2>();
+    fringe.add(search.startState);
+    Cube2 current = ((LinkedList<Cube2>) fringe).pollFirst();
+    Set<Cube2> visited = new HashSet<>();
+    int count = 0;
+    char lastAction;
+    while (!checkAllSuccess(current)) {
+        if(visited.contains(current)){
+          continue;
+        }
+
+        lastAction=current.actions.charAt(current.actions.length()-1);
+        if(lastAction=='0'){
+
+        }else if(catergorizeAction(lastAction)==1){
+
+        }else if(catergorizeAction(lastAction)==2){
+
+        }else if(catergorizeAction(lastAction)==3){
+
+        }
+
+
+
+
+      count++;
+      if (count % 1000 == 0) {
+        System.out.println("searched " + count + " times");
+      }
+    }
+    return current.actions;
+  }
+
+
+  public static boolean checkAllSuccess(Cube2 current) {
+    for (int i = 0; i < 6; i++) {
+      for (int j = 1; j < 4; j++) {
+        if (current.status[j - 1][i] != current.status[j][i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static int catergorizeAction(char c){
+    if(c=='R'||c=='r'||c=='L'||c=='l'){
+      return 1;
+    }
+    if(c=='F'||c=='f'||c=='B'||c=='b'){
+      return 2;
+    }
+    if(c=='U'||c=='u'||c=='D'||c=='d'){
+      return 3;
+    }
+    return 0;
   }
 }
