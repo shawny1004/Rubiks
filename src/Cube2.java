18a97f5eb78ea89962f2c2a03ba88fc826394315
Shuo Yang <yang.shuo2@husky.neu.edu>
2018-12-03 00:56:43 -0500

    a

--- src/Cube2.java
+++ src/Cube2.java
@@ -3,11 +3,8 @@ import static java.util.Objects.hash;
 import java.util.Random;
 
 public class Cube2 {
-
   //stores the 2*2*2 cube status
-
   int[][] status;
-  String actions;
   //initialize with
   //f:face c: cube
   //1111,2222,3333,4444,5555,6666
@@ -18,25 +15,52 @@ public class Cube2 {
         status[c][f] = f + 1;
       }
     }
-    actions = "0";
   }
 
-  public Cube2(int i){
+  public Cube2(int i) {
     status = new int[4][6];
-    actions = "0";
   }
 
-  public boolean equals(Object o){
-    if(!(o instanceof Cube2)){
+  public boolean equals(Object o) {
+    if (!(o instanceof Cube2)) {
       return false;
     }
-    Cube2 another = (Cube2)o;
-    return this.status.equals(another.status);
+    Cube2 another = (Cube2) o;
+    return transferToBasic(this).status.equals(transferToBasic(another).status);
   }
 
-  public int hashCode(){
-    return hash(this.status);
+  public int hashCode() {
+    return hash(transferToBasic(this).status);
+  }
+
+  public static Cube2 transferToBasic(Cube2 cube) {
+    Cube2 cubebasic = new Cube2();
+    int[] temp = new int[]{0, 0, 0, 0, 0, 0};
+    int count = 0;
+    for (int f = 0; f < 6; f++) {
+      for (int c = 0; c < 4; c++) {
+        if (temp[cube.status[c][f] - 1] == 0) {
+          count++;
+          temp[cube.status[c][f] - 1] = count;
+          cube.status[c][f] = count;
+        } else {
+          cube.status[c][f] = temp[cube.status[c][f] - 1];
+        }
+      }
+    }
+    cubebasic.status = cube.status;
+
+    return cubebasic;
+  }
+
+
+  public static void printCube(Cube2 cube) {
+    for (int i = 0; i < 6; i++) {
+      for (int j = 0; j < 4; j++) {
+        System.out.print(cube.status[j][i]);
+      }
+      System.out.println();
+    }
   }
 
 }
-
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ public class Main {
 
   public static void main(String[] args) {
     Cube2 c1 = new Cube2();
-    Search s1 = new Search(c1);
-    s1.shuffle();
-    Search.optmisticSearch(s1);
+    c1= Search.shift(c1,'R');
+    c1 = Search.shift(c1,'L');
+    c1 = Cube2.transferToBasic(c1);
+    Cube2.printCube(c1);
+
+    System.out.println();
+    Cube2 c2 = new Cube2();
+    c2= Search.shift(c2,'F');
+    c2 = Search.shift(c2,'B');
+    c2 = Cube2.transferToBasic(c2);
+    Cube2.printCube(c2);
+
   }
 }
